2f7378a7d6688424b9fdecabd415424176bf2787
Konsta <konsta.karjalainen@tuni.fi>
2023-12-04 21:17:59 +0200

    tests for add

--- test/component.test.js
+++ test/component.test.js
@@ -2,9 +2,24 @@ import chai from 'chai';
 const expect = chai.expect;
 import add from "../src/add.js"
 
-describe('Add.js tests', () => {
-    it('should sum two positive integers', () => {
-        const sum = add(6,4);
-        expect(sum).to.be.equal(10);
+describe('add.js tests', () => {
+    it('should add two numbers', () => {
+      const result = add(3, 4);
+      expect(result).to.equal(7);
+    });
+  
+    it('should add zero and a number', () => {
+      const result = add(0, 5);
+      expect(result).to.equal(5);
+    });
+  
+    it('should add a negative and a positive number', () => {
+      const result = add(-2, 8);
+      expect(result).to.equal(6);
+    });
+
+    it('should be zero when adding same negative and positive number', () => {
+        const result = add(-2, 2);
+        expect(result).to.equal(0);
     });
 });
